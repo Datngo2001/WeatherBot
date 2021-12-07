632094edc2ce46b735bb625cdb33ed166a1aa5c1
Đạt Ngô <ngominhdat110115@gmail.com>
2021-12-07 21:19:14 +0700

    done lambda

---
+++ my-bot/handler.js
@@ -0,0 +1,27 @@
+'use strict';
+const axios = require("axios");
+
+module.exports.getWeather = async (event) => {
+  const city = event.currentIntent.slots["City"];
+  const url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=08e3ad0437282f0abefa56ee74ab56af";
+
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+
+    const answer = "The temperature is " + data.main.temp + "C and " + data.weather[0].description + " is expected.";
+
+    return {
+      "dialogAction": {
+        "type": "Close",
+        "fulfillmentState": "Fulfilled",
+        "message": {
+          "contentType": "PlainText",
+          "content": answer
+        }
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
